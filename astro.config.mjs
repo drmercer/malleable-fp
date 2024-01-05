4d00ef76983a18eaf2752f9c3cc90539d6ddbec9
Dan Mercer <danielmercer1@gmail.com>
2024-01-04 22:16:58 -0700

    Deploy to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable Preact to support Preact JSX components.
-	integrations: [preact()],
+  // Enable Preact to support Preact JSX components.
+  integrations: [preact()],
+  output: "hybrid",
+  adapter: cloudflare()
 });
